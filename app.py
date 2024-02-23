4a9e5294d70c6888cca220e59198e467802be794
Darbortest <159553871+Darbortest@users.noreply.github.com>
2024-02-23 13:35:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,14 @@ import subprocess
 
 app = Flask(__name__)
 
-# Sta?e
 SSH_PASSWORD = "clico123!"
 SSH_USERNAME = "Student"
 SSH_HOST = "10.9.2.102"
 
 def execute_command(command):
-    # Przygotowanie pe?nej komendy z u?yciem sshpass
     full_command = 'sshpass -p {} ssh {}@{} "powershell {}"'.format(SSH_PASSWORD, SSH_USERNAME, SSH_HOST, command)
 
     try:
-        # Wykonanie komendy
         result_bytes = subprocess.check_output(full_command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
         return result_bytes
     except subprocess.CalledProcessError as e:
@@ -29,7 +26,6 @@ def index():
 def run_command():
     command = request.form["command"]
     
-    # Wywo?anie funkcji execute_command z przekazanym has?em
     result = execute_command(command)
     
     return result
